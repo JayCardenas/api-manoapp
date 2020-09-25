2ace55ded7fe0c55f32d1f6ffa97b231539e41aa
Jose Luis Cardenas Oscanoa <jolcarde@ucsc.edu>
2020-09-25 12:26:05 -0700

    comment out serilaze in app.js

--- app.js
+++ app.js
@@ -16,22 +16,22 @@ var db = new sqlite3.Database("./database/api_database.db", (err) => {
   console.log("Connected to the api SQlite database");
 }); // Databse object (the sqlite3.Database() returns obj with db connection automatically )
 
-db.serialize(() => {
-  // Queries scheduled here will be serialized.
-  db.run("CREATE TABLE greetings(message text)")
-    .run(
-      `INSERT INTO greetings(message)
-          VALUES('Hi'),
-                ('Hello'),
-                ('Welcome')`
-    )
-    .each(`SELECT message FROM greetings`, (err, row) => {
-      if (err) {
-        throw err;
-      }
-      console.log(row.message);
-    });
-});
+// db.serialize(() => {
+//   // Queries scheduled here will be serialized.
+//   db.run("CREATE TABLE greetings(message text)")
+//     .run(
+//       `INSERT INTO greetings(message)
+//           VALUES('Hi'),
+//                 ('Hello'),
+//                 ('Welcome')`
+//     )
+//     .each(`SELECT message FROM greetings`, (err, row) => {
+//       if (err) {
+//         throw err;
+//       }
+//       console.log(row.message);
+//     });
+// });
 // close database connection
 db.close((err) => {
   if (err) {
